a6f4b4a46e517baa26fe565ad35e4508b2d95e17
Robert Feldt <robert.feldt@gmail.com>
2008-06-29 13:32:22 +0200

    added a test dir with files and then speced the files-below func

--- clojure/specs/file_watch_spec.clj
+++ clojure/specs/file_watch_spec.clj
@@ -16,6 +16,19 @@
     (is = [:k] (keys-with-different-values {:k "a"} {:k "b"}))
   )
   (spec "complex example"
-    (is = [:k1 :k3 :k4] (keys-with-different-values {:k1 "a" :k2 "b" :k3 1} {:k1 "a2" :k2 "b" :k4 2}))
+    (is = [:k1 :k3 :k4] (keys-with-different-values {:k1 :a :k2 :b :k3 1}
+                                                    {:k1 :a2 :k2 :b :k4 2}))
   )
 )
+
+(spec files-below
+  ;; TODO: access the dir where this file lives in so we can make
+  ;; the dir ref below location-independent
+  (let [testdir (new java.io.File "specs/data/a-directory")]
+    (is = (map (fn [f] (.getPath f)) (files-below testdir))
+          ["f1" "f2" "a-sub-dir/f3"])
+  )
+)
+
+;; TODO: to test last-modified-map-below etc we need a way to add and 
+;; delete files.
